e505cb6ab766fe9dc00b87e25da5db23dda75fd4
Bhojarajperua <135651770+Bhojarajperua@users.noreply.github.com>
2025-03-15 01:11:07 +0530

    Update Hello.js

--- Hello.js
+++ Hello.js
@@ -1,8 +1,8 @@
 document.getElementById("lagButton").addEventListener("click", function() {
-    document.getElementById("status").textContent = "Freezing for 30 seconds...";
+    document.getElementById("status").textContent = "Freezing for 5 seconds...";
     
     let start = Date.now();
-    while (Date.now() - start < 30000); // Busy loop for 5 seconds
+    while (Date.now() - start < 5000); // Busy loop for 5 seconds
     
     document.getElementById("status").textContent = "You Are My Friend Now!";
 });
